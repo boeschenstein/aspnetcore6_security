6d75a712685c292f72827ed54fd843cbcd742748
Boeschenstein, Patrik <patrik.boeschenstein@hotmail.com>
2022-01-09 17:25:02 +0100

    .

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -19,6 +19,8 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        //[AllowAnonymous]
+
         [Authorize(Roles = "dummy_2")] // access
         //[Authorize(Roles = "dummy_3")] // no access
 
