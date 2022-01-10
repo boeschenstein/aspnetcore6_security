b981842915666935d39983b77c16b9cea93d67b4
Boeschenstein, Patrik <patrik.boeschenstein@hotmail.com>
2022-01-10 16:37:34 +0100

    add claim-based demo

---
+++ claim-based/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[AllowAnonymous]
+        [Authorize(Policy = "EmployeeOnly")] // access
+        //[Authorize(Policy = "unknown_policy")] // no access
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ claim-based/WebApplication1/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// config Kestrel to require certificate (only for debug)
+// https://docs.microsoft.com/en-us/aspnet/core/security/authentication/certauth?view=aspnetcore-6.0#configure-your-server-to-require-certificates
+//builder.WebHost.ConfigureKestrel(o =>
+//{
+//    // todo: how to create a client certificate?
+//    //o.ConfigureHttpsDefaults(o => o.ClientCertificateMode = ClientCertificateMode.RequireCertificate);
+//});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+
+    options.AddPolicy("EmployeeOnly", policy => policy.RequireClaim("EmployeeNumber"));
+});
+
+//builder.Services.AddRazorPages(); // todo: for what is this?
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseCustomWindowsUser(); // middleware is getting ClaimsPrincipal + ClaimsIdentity instead of WindowsPrincipal+WindowsIdentity!
+
+app.UseAuthentication();
+
+app.UseCustomWindowsUser(); // after app.UseAuthentication() ! middleware is getting WindowsPrincipal+WindowsIdentity (on the second execution)
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
